dd43116ec93365cf01fa42279c516c77d669c563
dkkim0122 <dkkim0122@gmail.com>
2022-02-07 13:45:00 +0900

    zoom video finished

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,6 +16,8 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
     try {
@@ -86,6 +88,13 @@ function handleCameraClick(){
 
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if(myPeerConnection){
+        const videoTrack = myStream.getVideoTracks()[0]; // 지금 내 브라우저에서의 비디오 트랙
+        const videoSender = myPeerConnection
+            .getSenders()
+            .find(sender => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack); // 그 트랙을 똑같이 다른 peer에게 보낸다.
+    }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -98,17 +107,19 @@ const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
 
-function startMedia(){
+async function initCall(){
     welcome.hidden = true;
     call.hidden = false;
-    getMedia();
+    await getMedia();
+    makeConnection();
 }
 
-function handleWelcomeSubmit(event){
+async function handleWelcomeSubmit(event){
     event.preventDefault();
     const input = welcomeForm.querySelector("input");
     const value = input.value;
-    socket.emit("join_room", value, startMedia);
+    await initCall(); // 수정됨
+    socket.emit("join_room", value);
     roomName = input.value;
     input.value = "";
 }
@@ -116,6 +127,68 @@ function handleWelcomeSubmit(event){
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
-socket.on("welcome", () => {
-    console.log("someone joined");
+/* peer A가 이미 방에 있고 peer B가 이 방에 들어오면
+   peer A에서 실행되는 코드 */
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer(); // 다른 브라우저가 초대될 수 있는 초대장을 만든다. 
+    myPeerConnection.setLocalDescription(offer); // peerA가 이 offer로 connection을 구성한다.
+    console.log("sent an offer")
+    socket.emit("offer", offer, roomName);
 })
+
+/* peer A에서 실행되는 코드 */
+socket.on("answer", answer => {
+     myPeerConnection.setRemoteDescription(answer);
+     console.log("recieved the answer");
+})
+
+/* peer B에서 실행되는 코드 */
+socket.on("offer", async (offer) => {
+    console.log("recieved the offer");
+    myPeerConnection.setRemoteDescription(offer);  // peer A가 보낸 description 받아서 setting하기
+    const answer = await myPeerConnection.createAnswer(); 
+    myPeerConnection.setLocalDescription(answer);  // local description 만들기
+
+    socket.emit("answer", answer, roomName); // peer B가 peer A로 뭔가 보낼 일이 있을 때 answer를 보낸다.
+    console.log("sent the answer");
+})
+
+socket.on("ice", ice => {
+    console.log("recieve ice candidate")
+    myPeerConnection.addIceCandidate(ice);
+})
+
+// RTC code
+
+function makeConnection() {
+    /* 양쪽 브라우저에서 각각 peer to peer connection을 만들고
+       브라우저의 오디오, 비디오 스트림을 peer to peer connection에 집어넣기(아직 연결은 x) */
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ]
+            }
+        ]
+    });
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    myStream
+        .getTracks()
+        .forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+
+/* 브라우저들이 ice candidate를 서로 주고받는다. */
+function handleIce(data) {
+    socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data){
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+}
--- src/server.js
+++ src/server.js
@@ -27,11 +27,19 @@ instrument(wsServer, {
 
 
 wsServer.on("connection", socket => {
-    socket.on("join_room", (roomName, done) => {
+    socket.on("join_room", (roomName) => {
         socket.join(roomName);
-        done();
         socket.to(roomName).emit("welcome");
     }) 
+    socket.on("offer", (offer, roomName) => { // peer A에게서 offer를 받고 peer B에 emit
+        socket.to(roomName).emit("offer", offer);
+    })
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    })
+    socket.on("ice", (ice, roomName)=> {
+        socket.to(roomName).emit("ice", ice);
+    })
 })
 
 
